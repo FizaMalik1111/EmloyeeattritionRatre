142ef6de02771a5dab09e270baf171f4fe2343a6
Fiza Malik <fiza95028@gmail.com>
2024-08-07 10:45:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ import numpy as np
 import pickle
 
 # Load the model
-clf = pickle.load(open("case_study_emp.pkl","rb"))
+clf = pickle.load(open("case_study_emp.pkl","rw"))
 
 def predict(data):
+	clf = pickle.load(open("case_study_emp.pkl","rw"))
 	return clf.predict(data)
 
 
@@ -35,4 +36,4 @@ if st.button("Predict Performance Rate"):
     result = clf.predict(
         np.array([[el,tos,top,gr,gr1,1,1,1,1,1,0]]))
     st.text(result[0])
-st.markdown("Developed by Pratha")
+st.markdown("Developed by Fiza")
